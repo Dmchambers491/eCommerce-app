088204853806ccae00a566313641f6a2394ad547
Darreal Chambers <dchambers@Darreals-MBP.lan1>
2020-11-03 15:51:12 -0500

    delete and put mapping created

--- src/main/java/com/cognixia/jump/controller/UserController.java
+++ src/main/java/com/cognixia/jump/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.cognixia.jump.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cognixia.jump.exception.InvalidLoginPageException;
@@ -42,6 +47,35 @@ public class UserController {
 		return user;
 	}
 	
+	@DeleteMapping("/delete/user/{id}")
+	public ResponseEntity<String> deleteUser(@PathVariable long id) {
+		
+		Optional<User> found = service.findById(id);
+		
+		if(found.isPresent()) {
+			service.deleteById(id);
+			
+			return ResponseEntity.status(200).body("User has been deleted");
+			
+		}else {
+			return ResponseEntity.status(400).header("User id", id + "").body("User not found");
+		}
+	}
+	
+	@PutMapping("/update/user")
+	public @ResponseBody String updateUser(@RequestBody User updateUser) {
+		
+		Optional<User> found = service.findById(updateUser.getId());
+		
+		if(found.isPresent()) {
+			service.save(updateUser);
+			return "Saved: " + updateUser.toString();
+		}else {
+			return "Could not update user";
+		}
+		
+	}
+	
 	@PostMapping("/add/user")
 	public void addUser(@RequestBody User user) {
 		
@@ -52,4 +86,6 @@ public class UserController {
 		System.out.println("Added: " + added);
 		
 	}
+	
+	
 }
